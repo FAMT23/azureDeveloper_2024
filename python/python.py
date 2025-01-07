461bb5744d6fca1ede3b9edf6ab0c5a4a8cabbd7
FAMT23 <122385732+FAMT23@users.noreply.github.com>
2025-01-07 18:17:01 +0000

    Empenzando nuevo curso básico de Python. Variables y f-string

---
+++ python/python.py
@@ -0,0 +1,18 @@
+import os
+os.system('clear')
+
+mensaje_suma="El resultado de la suma es: "
+mensaje_producto="El mensaje de la multiplicacion es: "
+suma = 1+2
+producto = suma * 2
+print("El resultado de la suma es" + str(sum))
+print("El resultado de la multiplicación es: "+ str(producto))
+
+nombre = "Juan"
+edad = 30
+print(f"Hola, {nombre}. Tienes {edad} años")
+
+pi = 3.14159265
+print(f"El valor de pi es: {pi:.4f}")
+
+print(f"Pi es del tipo {type(pi)}")
